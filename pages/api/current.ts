23e3d9245e8aa7385ea72e6befcb9faf9d9ee813
RJD07 <147194017+RJD07@users.noreply.github.com>
2023-12-10 02:22:20 +0800

    current update

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -9,10 +9,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const { currentUser } = await serverAuth(req, res);
+    console.log('Authentication successful:', currentUser);
 
     return res.status(200).json(currentUser);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).end();
+  } catch (error: any) { // Explicitly typing error as 'any' or 'Error'
+    console.error('Error in /api/current:', error);
+    return res.status(400).json({ error: error.message || 'An error occurred' });
   }
 }
