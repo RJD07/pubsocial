707e6f6da12b2449d0699844d6466df9a711f609
RJD07 <147194017+RJD07@users.noreply.github.com>
2023-12-10 01:58:40 +0800

    post updates

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -1,6 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/libs/prismadb";
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).end();
@@ -8,6 +5,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const { postId } = req.query;
+
+    console.log('postId:', postId);
 
     if (!postId || typeof postId !== 'string') {
       throw new Error('Invalid ID');
@@ -30,13 +29,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
     });
 
-    if (!post) {
-      return res.status(404).json({ error: 'Post not found' });
-    }
-
     return res.status(200).json(post);
   } catch (error) {
-    console.error('Error in fuckyou:', error);
-    return res.status(400).end();
+    console.error(error);
+    return res.status(500).end();
   }
 }
