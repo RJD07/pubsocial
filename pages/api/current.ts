80c906879b11f8dc284f360ee5ab1056744d6792
RJD07 <147194017+RJD07@users.noreply.github.com>
2023-12-10 02:36:14 +0800

    Update current.ts

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -2,22 +2,17 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import serverAuth from '@/libs/serverAuth';
 
-const handleGetRequest = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { currentUser } = await serverAuth(req, res);
-    console.log('Authentication successful:', currentUser);
-
-    return res.status(200).json(currentUser);
-  } catch (error: any) { // or catch (error: Error)
-    console.error('Error in /api/current:', error);
-    return res.status(400).json({ error: (error as Error).message || 'An error occurred' });
-  }
-};
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).end();
   }
 
-  return await handleGetRequest(req, res);
+  try {
+    const { currentUser } = await serverAuth(req, res);
+
+    return res.status(200).json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
 }
